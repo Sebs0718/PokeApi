ed230a510875cc0e6f23606bba1d426ee6a30e9b
Sebastian Sandoval polo <polosandovalsebastian@gmail.com>
2021-08-24 11:42:08 -0500

    Filter Location

--- src/pages/Locations.js
+++ src/pages/Locations.js
@@ -4,12 +4,14 @@ import { useSelector } from 'react-redux';
 import Axios from 'axios';
 import { Link } from 'react-router-dom';
 import * as AiIcons from 'react-icons/ai';
+import FlastList from 'flatlist-react';
 
 function Locations (){
 
     const sidebar = useSelector(state => state);
     const [pagintate, setPaginate] = useState(0);
     const [locations, setLocations] = useState([]);
+    const [search, setSearch] = useState('');
 
     const getLocations = async ()=>{
         try {
@@ -30,6 +32,10 @@ function Locations (){
         getLocations();
     }
 
+    const getValueSearch = (e)=>{
+        setSearch(e.target.value)
+    }
+
     useEffect(()=>{
         getLocations();
     },[])
@@ -42,27 +48,45 @@ function Locations (){
                     <h1>Locaciones</h1>
                 </div>
                 <div className='container body'>
-                    <div className='row' >
-                        {locations.map((item,index)=>{
-                            if(index >= pagintate && index <= pagintate + 10){
-                                return(
-                                    <div className='col-md-4 my-3' key={index}>
-                                        <div className='card'>
-                                            <h6>{item.name}</h6>
-                                            <Link to={`/LocationMore/${index + 1}`} >
-                                                <button className='btn btn-success btn-sm'> Ver mas</button>
-                                            </Link>
+                    <div className='row'>
+                        <h6 className='offset-md-1 col-md-6'>Busqueda de Pokémon</h6>
+                        <input type='text' className='col-md-8 offset-md-1' id='search' placeholder='Buscar' onChange={getValueSearch} value={search}/>
+                        <FlastList 
+                            list={search != "" ? locations.filter((text) =>  text.name.toLowerCase().includes(search)) : locations}
+                            renderItem={(item,index)=>{
+                                if(search != ""){
+                                    return(
+                                        <div className='col-md-4 my-3' key={index}>
+                                            <div className='card'>
+                                                <h6>{item.name}</h6>
+                                                <Link to={`/LocationMore/${index + 1}`} >
+                                                    <button className='btn btn-success btn-sm'> Ver mas</button>
+                                                </Link>
+                                            </div>
                                         </div>
-                                    </div>
-                                );
-                            }
-                        })}
+                                    );
+                                }else{
+                                    if(index >= pagintate && index <= pagintate + 10){
+                                        return(
+                                            <div className='col-md-4 my-3' key={index}>
+                                                <div className='card'>
+                                                    <h6>{item.name}</h6>
+                                                    <Link to={`/LocationMore/${index + 1}`} >
+                                                        <button className='btn btn-success btn-sm'> Ver mas</button>
+                                                    </Link>
+                                                </div>
+                                            </div>
+                                        );
+                                    }
+                                }
+                            }}
+                        />
                     </div>
                 </div>
-                <div className='container footer'>
-                    <h6 className='mx-3'>Resultados {(pagintate + 1) + "-" + (pagintate + 10)}</h6>
-                    <button className='btn btn-sm btn-primary' onClick={pagintate >= 10 ? previousPaginate : ()=>{}}> <AiIcons.AiOutlineArrowLeft /> </button>
-                    <button className='btn btn-sm btn-primary mx-2' onClick={pagintate < 786 ?nextPaginate : ()=>{}}> <AiIcons.AiOutlineArrowRight /> </button>
+                <div className='container footer my-2'>
+                    <h6 className='mx-3'>{search != "" ? "Todos los resultados" : "Resultados " + (pagintate + 1) + "-" + (pagintate + 10)}</h6>
+                    <button className='btn btn-sm btn-primary' onClick={pagintate >= 10 ? previousPaginate : ()=>{}} disabled={search != "" ? true : false}> <AiIcons.AiOutlineArrowLeft /> </button>
+                    <button className='btn btn-sm btn-primary mx-2' onClick={pagintate < 786 ?nextPaginate : ()=>{}} disabled={search != "" ? true : false}> <AiIcons.AiOutlineArrowRight /> </button>
                 </div>
             </div >
         </div>
